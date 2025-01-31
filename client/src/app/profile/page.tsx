18464aa5d9b567f742d485fb8d7feba43834abb7
rupesh <rpiwal02@gmail.com>
2025-02-01 00:07:05 +0530

    add: profile page

---
+++ client/src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+export default function ProfilePage() {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold">Profile</h1>
+        <p className="mt-4">This is the Profile page.</p>
+      </div>
+    )
+  }
+  
+  
