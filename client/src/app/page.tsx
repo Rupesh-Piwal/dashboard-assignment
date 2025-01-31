bbd5be4fbbc4f39f33bed51ead0d6100941a071a
rupesh <rpiwal02@gmail.com>
2025-01-31 17:06:30 +0530

    project intialization

---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div className="space-y-8">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">Dashboard</h1>
+      </div>
+    </div>
+  );
+}
